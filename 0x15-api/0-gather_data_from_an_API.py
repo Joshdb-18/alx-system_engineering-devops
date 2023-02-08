e90cc38e140b79497878bc1018024928e09c4250
Joshdb-18 <joshuadami17@gmail.com>
2023-02-08 16:28:36 +0100

    collect info from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Returns the information about an employee's TODO list progress"""
-import sys
 import requests
+import sys
 
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
