b47979f3196c9b1209fe964bd31b3a6cbbba8bb9
Joshdb-18 <joshuadami17@gmail.com>
2023-02-28 12:42:30 +0100

    Count it..

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,44 +5,46 @@
 import requests
 
 
-def count_words(subreddit, word_list, after=None, counts=None):
-    """A function that queries the Reddit API parses the title of
+def count_words(subreddit, word_list, after='', word_dict={}):
+    """ A function that queries the Reddit API parses the title of
     all hot articles, and prints a sorted count of given keywords
     (case-insensitive, delimited by spaces.
     Javascript should count as javascript, but java should not).
     If no posts match or the subreddit is invalid, it prints nothing.
     """
-    if counts is None:
-        counts = {}
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    if after:
-        url += f"&after={after}"
+    if not word_dict:
+        for word in word_list:
+            if word.lower() not in word_dict:
+                word_dict[word.lower()] = 0
 
-    headers = {"User-Agent": "reddit-client"}
-    response = requests.get(url, headers=headers)
+    if after is None:
+        wordict = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
+        for word in wordict:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
+        return None
+
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    header = {'user-agent': 'redquery'}
+    parameters = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=header, params=parameters,
+                            allow_redirects=False)
 
     if response.status_code != 200:
-        return
+        return None
 
-    articles = response.json()["data"]["children"]
-    last_article = articles[-1]["data"] if articles else None
-    current_counts = counts.copy()
+    try:
+        hot = response.json()['data']['children']
+        aft = response.json()['data']['after']
+        for post in hot:
+            title = post['data']['title']
+            lower = [word.lower() for word in title.split(' ')]
 
-    for article in articles:
-        title = article["data"]["title"].lower()
-        title_words = [word.strip("!.,_") for word in title.split()]
+            for word in word_dict.keys():
+                word_dict[word] += lower.count(word)
 
-        for keyword in word_list:
-            if keyword.lower() in title_words:
-                current_counts[keyword.lower()] = (
-                    current_counts.get(keyword.lower(), 0) + 1
-                )
+    except Exception:
+        return None
 
-    if last_article:
-        return count_words(subreddit, word_list,
-                           last_article["name"], current_counts)
-
-    sorted_counts = sorted(current_counts.items(), key=lambda x: (-x[1], x[0]))
-    for keyword, count in sorted_counts:
-        print(f"{keyword}: {count}")
+    count_words(subreddit, word_list, aft, word_dict)
